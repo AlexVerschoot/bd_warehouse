8a065741df91ab3dc106bdfb91a547052eae9f00
Alex Verschoot <alexverschoot@gmail.com>
2025-12-27 19:42:40 +0100

    added the grooves

--- src/bd_warehouse/aluminum_extrusions.py
+++ src/bd_warehouse/aluminum_extrusions.py
@@ -11,7 +11,7 @@ desc:
 
 """
 
-from build123d import Align, BaseSketchObject, BuildLine, BuildSketch, Literal, Mode, RectangleRounded, FilletPolyline
+from build123d import Align, BaseSketchObject, BuildLine, BuildSketch, Literal, Location, Locations, Mode, Plane, Polyline, RectangleRounded, FilletPolyline, make_face, mirror
 import csv
 import importlib.resources as pkg_resources
 import bd_warehouse
@@ -25,7 +25,8 @@ class AluminiumExtrusionIType(BaseSketchObject):
         for row in reader:
             if len(row) == 0:  # skip blank rows
                 continue
-            name, width,height,corner_radius,hole_dia,flange_thickness,flange_opening,flange_depth,source = row
+            name, width, height, corner_radius, hole_dia, flange_thickness, flange_opening, flange_max_width, flange_depth, flange_bottom_radius, flange_neck_top_radius, flange_neck_bottom_radius, flange_top_radius, source = row
+
             extrusionData[name] = {}
             extrusionData[name]['width'] = float(width)
             extrusionData[name]['height'] = float(height)
@@ -33,7 +34,14 @@ class AluminiumExtrusionIType(BaseSketchObject):
             extrusionData[name]['hole_dia'] = float(hole_dia)
             extrusionData[name]['flange_thickness'] = float(flange_thickness)
             extrusionData[name]['flange_opening'] = float(flange_opening)
+            extrusionData[name]['flange_max_width'] = float(flange_max_width)
             extrusionData[name]['flange_depth'] = float(flange_depth)
+            extrusionData[name]['flange_bottom_radius'] = float(flange_bottom_radius)
+            extrusionData[name]['flange_neck_top_radius'] = float(flange_neck_top_radius)
+            extrusionData[name]['flange_neck_bottom_radius'] = float(flange_neck_bottom_radius)
+            extrusionData[name]['flange_top_radius'] = float(flange_top_radius)
+            extrusionData[name]['source'] = source
+
 
     def __init__(
         self,
@@ -42,14 +50,57 @@ class AluminiumExtrusionIType(BaseSketchObject):
         align: tuple[Align, Align] = (Align.CENTER, Align.CENTER),
         mode: Mode = Mode.ADD,
     ):
-        _applies_to = [BuildSketch._tag]
+        #_applies_to = [BuildSketch._tag]
         with BuildSketch() as mainSketch:
+            #base rectangle
             RectangleRounded(
                 width=self.extrusionData[extrusion_type]['width'],
                 height=self.extrusionData[extrusion_type]['height'],
                 radius=self.extrusionData[extrusion_type]['corner_radius']
             )
-            
+            groove_placements = [
+                ((0, -self.extrusionData[extrusion_type]['height'] / 2), 0),
+                ((0,  self.extrusionData[extrusion_type]['height'] / 2), 180),
+                ((-self.extrusionData[extrusion_type]['width'] / 2, 0), -90),
+                (( self.extrusionData[extrusion_type]['width'] / 2, 0), 90),
+            ]
+            for (x, y), rot in groove_placements:
+                with BuildLine(Location((x,y,0),angle=rot)) as grooves:
+                    slot_groove_points = [
+                        #extra start in line with the profile to make easy entry fillet
+                        (self.extrusionData[extrusion_type]['flange_opening']*3/4, -1),
+                        (self.extrusionData[extrusion_type]['flange_opening']*3/4, 0),
+                        #the real figure starts
+                        (self.extrusionData[extrusion_type]['flange_opening']/2, 0),
+                        (self.extrusionData[extrusion_type]['flange_opening']/2, self.extrusionData[extrusion_type]['flange_thickness']),
+                        (self.extrusionData[extrusion_type]['flange_max_width']/2, self.extrusionData[extrusion_type]['flange_thickness']),
+                        (self.extrusionData[extrusion_type]['flange_opening']/2, self.extrusionData[extrusion_type]['flange_depth']),
+                        #(0, self.extrusionData[extrusion_type]['flange_depth']),
+                        (-self.extrusionData[extrusion_type]['flange_opening']/2, self.extrusionData[extrusion_type]['flange_depth']),
+                        (-self.extrusionData[extrusion_type]['flange_max_width']/2, self.extrusionData[extrusion_type]['flange_thickness']),
+                        (-self.extrusionData[extrusion_type]['flange_opening']/2, self.extrusionData[extrusion_type]['flange_thickness']),
+                        (-self.extrusionData[extrusion_type]['flange_opening']/2, 0),
+                        #the real figure ends
+                        (-self.extrusionData[extrusion_type]['flange_opening']*3/4, 0),
+                        (-self.extrusionData[extrusion_type]['flange_opening']*3/4, -1),
+
+                    ]
+                    slot_groove_radii = [
+                        0,
+                        0,
+                        self.extrusionData[extrusion_type]['flange_neck_top_radius'],
+                        self.extrusionData[extrusion_type]['flange_neck_bottom_radius'],
+                        self.extrusionData[extrusion_type]['flange_top_radius'],
+                        self.extrusionData[extrusion_type]['flange_bottom_radius'],
+                        self.extrusionData[extrusion_type]['flange_bottom_radius'],
+                        self.extrusionData[extrusion_type]['flange_top_radius'],
+                        self.extrusionData[extrusion_type]['flange_neck_bottom_radius'],
+                        self.extrusionData[extrusion_type]['flange_neck_top_radius'],
+                        0,
+                        0
+                    ]
+                    fpl = FilletPolyline(slot_groove_points, radius=slot_groove_radii, close=True, mode=Mode.ADD)
+                make_face(edges=grooves.edges(), mode=Mode.SUBTRACT)
         
         super().__init__(obj=mainSketch.sketch, rotation=rotation, align=align, mode=mode)
 
